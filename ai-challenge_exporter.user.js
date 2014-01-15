c9b791f33ffb8498ce1eb68d325d0f069974efa5
KOLANICH <kolan_n@mail.ru>
2014-01-15 16:19:32 +0400

    added import

--- ai-challenge_exporter.user.js
+++ ai-challenge_exporter.user.js
@@ -8,6 +8,7 @@
 // @description			Helps you export all the files from ai-challenge.com into zip archive
 // @license				GNU GPL v3
 // @noframes			1
+// @icon				http://ai-challenge.com/favicon.ico
 // @website				https://github.com/KOLANICH/ai-challenge-Exporter
 // @include				http://ai-challenge.com/ru/profile/*
 // @resource			jszip	https://raw.github.com/Stuk/jszip/master/jszip.js
@@ -16,7 +17,11 @@
 // @contributionAmount	feel free to fork and contribute
 // ==/UserScript==
 //var unpackCodesRx=/([a-f\d]{32})(\w+\.\w+)\1/ig;
-const unpackCodesRx=/89c4e347cdca08508a415e959f2a9b5c(\w+\.\w+)89c4e347cdca08508a415e959f2a9b5c/ig;
+const signature="89c4e347cdca08508a415e959f2a9b5c";
+const unpackCodesRx=new RegExp("(?://)?(?:\\r?\\n)*"+signature+"(\\w+\\.\\w+)"+signature+"(?:\\r?\\n)*","ig");
+var fileEl= document.createElement("INPUT");
+fileEl.type="file";
+fileEl.accept="application/javascript, text/javascript";
 function checkInjection(){
 	if(typeof JSZip==="undefined")
 		eval(GM_getResourceText('jszip').replace("var JSZip","JSZip"));
@@ -44,6 +49,14 @@ function unpackCodes(str){
 	}
 	return filez;
 }
+function packCodes(filez,pretty){
+	if(typeof pretty=="undefined")pretty=true;
+	let str="";
+	for(let name in filez){
+		str+=(pretty?"//":"")+signature+name+signature+(pretty?"\n"+filez[name].code+"\n":encodeURIComponent(filez[name].code));
+	}
+	return str;
+}
 
 function exportToZip(filez){
 	checkInjection();
@@ -56,6 +69,33 @@ function exportToZip(filez){
 	console.log(blob);
 	return URL.createObjectURL(blob);
 }
+
+function importFromString(codes){
+	codes=unpackCodes(codes);
+	codes=packCodes(codes,false);
+	var ret = GM_xmlhttpRequest({
+		method: "POST",
+		data: "data="+encodeURIComponent(codes),
+		url : "/upload",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		onload: function(res) {
+			if (res.responseText == "ok") {
+				window.location.reload();
+				GM_notification("Код сохранен!");
+			} else {
+				GM_notification("Код не сохранен! Ошибка: " + res.responseText);
+			}
+		}
+	});
+}
+
+GM_registerMenuCommand("export to clipboard",function(){
+	let filez=unpackCodes(decodeURIComponent(unsafeWindow["tournamentView"].model.attributes.code));
+	GM_setClipboard(packCodes(filez),"text");
+	alert("Exported to clipboard");
+});
 
 GM_registerMenuCommand("export",function(){
 	let filez=unpackCodes(decodeURIComponent(unsafeWindow["tournamentView"].model.attributes.code));
@@ -72,4 +112,31 @@ GM_registerMenuCommand("export from editor",function(){
 	}
 	let codePacked=decodeURIComponent(unsafeWindow["tournamentView"].model.attributes.code);
 	console.log(unpackCodes(codePacked));*/
+});
+
+GM_registerMenuCommand("import from string",function(){
+	let codes=prompt("Input string");
+	if(!codes)return;
+	importFromString(codes);
+});
+
+var reader = new FileReader();
+reader.onload =function(evt){
+	let codes=evt.target.result;
+	if(!codes)return;
+	importFromString(codes);
+};
+function handleFileSelect(evt) {
+	for (let f of evt.target.files) {
+		reader.readAsText(f);
+		break;
+	}
+}
+
+fileEl.addEventListener('change', handleFileSelect, false);
+
+GM_registerMenuCommand("import from file",function(){
+	var evt = document.createEvent("MouseEvents");
+	evt.initMouseEvent("click", true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+	fileEl.dispatchEvent(evt);
 });
